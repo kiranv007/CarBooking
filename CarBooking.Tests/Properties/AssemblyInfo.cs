19e5de4f6b1eb927afad17371c1e1cecbf846b6b
kiranv007 <Kiran.V@nttdata.com>
2024-04-18 18:30:56 +0530

    Add project files.

---
+++ CarBooking.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CarBooking.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("NTT Data")]
+[assembly: AssemblyProduct("CarBooking.Tests")]
+[assembly: AssemblyCopyright("Copyright © NTT Data 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6ac3dee6-ce18-4f7c-8d40-3f3123c86fdc")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CarBooking/Controllers/CustomerLoginsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CarBooking.Models;
+
+namespace CarBooking.Controllers
+{
+    public class CustomerLoginsController : Controller
+    {
+        private CasestudyEntities db = new CasestudyEntities();
+
+        // GET: CustomerLogins
+        public ActionResult Index()
+        {
+            var customerLogins = db.CustomerLogins.Include(c => c.CustomerDetail);
+            return View(customerLogins.ToList());
+        }
+
+        // GET: CustomerLogins/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CustomerLogin customerLogin = db.CustomerLogins.Find(id);
+            if (customerLogin == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customerLogin);
+        }
+
+        // GET: CustomerLogins/Create
+        public ActionResult Create()
+        {
+            ViewBag.Username = new SelectList(db.CustomerDetails, "UserName", "FirstName");
+            return View();
+        }
+
+        // POST: CustomerLogins/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Username,Password")] CustomerLogin customerLogin)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CustomerLogins.Add(customerLogin);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Username = new SelectList(db.CustomerDetails, "UserName", "FirstName", customerLogin.Username);
+            return View(customerLogin);
+        }
+
+        // GET: CustomerLogins/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CustomerLogin customerLogin = db.CustomerLogins.Find(id);
+            if (customerLogin == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Username = new SelectList(db.CustomerDetails, "UserName", "FirstName", customerLogin.Username);
+            return View(customerLogin);
+        }
+
+        // POST: CustomerLogins/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Username,Password")] CustomerLogin customerLogin)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(customerLogin).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Username = new SelectList(db.CustomerDetails, "UserName", "FirstName", customerLogin.Username);
+            return View(customerLogin);
+        }
+
+        // GET: CustomerLogins/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CustomerLogin customerLogin = db.CustomerLogins.Find(id);
+            if (customerLogin == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customerLogin);
+        }
+
+        // POST: CustomerLogins/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            CustomerLogin customerLogin = db.CustomerLogins.Find(id);
+            db.CustomerLogins.Remove(customerLogin);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
